3ec38be3ffaecd2027c7edcec3b55bf7bd3f9e9d
moscowj3rk <mityafotografer@gmail.com>
2026-08-12 22:47:08 +0300

    first commit

---
+++ src/main/java/io/fateproject/fstaffwork/StaffWorkPlugin.java
@@ -0,0 +1,17 @@
+package io.fateproject.fstaffwork;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class StaffWorkPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
